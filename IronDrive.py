c739130387de8a39bc58b6114f4eec4377f65c60
Heriberto Brill Nonemacher <heriberto@khomp.com>
2020-10-03 21:39:15 -0300

    Monitoring HFSS

--- IronDrive.py
+++ IronDrive.py
@@ -7,6 +7,7 @@ Created on Thu Aug 27 19:54:53 2020
 
 import os
 import json
+import threading
 
 def status(message):
     
@@ -40,3 +41,22 @@ def status(message):
     #call the python script to send a message
     command = p['activated.bat'] + ' ' + p['anaconda3'] + ' && python C:/Send2Gchat/Send2Gchat.py'
     os.system(command)
+
+
+def callcheck():   
+    filepath = os.path.join('c:/Send2Gchat', 'config.json')
+    if not os.path.exists('c:/Send2Gchat'):
+        os.makedirs('c:/Send2Gchat')
+    
+    #This will open json config to upload the url and thread if that exists
+    with open(filepath) as json_file:
+        data = json.load(json_file)
+        for p in data['config']:
+            print('')
+    
+    command = p['activated.bat'] + ' ' + p['anaconda3'] + ' && python C:/Send2Gchat/checkprocess.py'
+    os.system(command)
+    
+def enableHFSSmonitor():
+    t = threading.Timer(1,callcheck)
+    t.start()
---
+++ checkprocess.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 20:55:30 2020
+
+@author: Heriberto
+"""
+import psutil,time
+
+import sys
+sys.path.append(r'C:/Send2Gchat')
+import IronDrive
+
+def check():
+    st = "no"
+    # time.sleep(60)
+    for p in psutil.process_iter(attrs=['pid', 'name']):
+        if p.info['name'] == "chrome.exe":
+            st = "yes"
+            break
+            #print("yes", (p.info['name']))
+    return st
+
+print("Monitoring ANSYS HFSS!!!")
+while(1):
+    st = check()
+    if st == "no":
+        break
+    time.sleep(5)#900
+IronDrive.status("HFSS was closed!!!")
+print("HFSS was closed!!!")
